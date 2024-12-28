03f4338d28d3c1ab5c44e116df32d2918f82e7d0
Cephas <cephaskiprono@gmail.com>
2024-12-29 00:02:23 +0300

    _______

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -7,7 +7,6 @@ import Lottie from "react-lottie";
 import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import { IoCopyOutline } from "react-icons/io5";
-// import GridGlobe from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -165,16 +164,11 @@ export const BentoGridItem = ({
 
           {id === 6 && (
             <div className="mt-5 relative">
-              {/* button border magic from tailwind css buttons  */}
-              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-              {/* add handleCopy() for the copy the text */}
               <div
                 className={`absolute -bottom-5 right-0 ${
                   copied ? "block" : "block"
                 }`}
               >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
